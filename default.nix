4270a825f3894eb0d7f9c38b3e712b8a33944413
Silvan Mosberger <silvan.mosberger@tweag.io>
2024-07-17 04:16:44 +0200

    Various improvements

--- default.nix
+++ default.nix
@@ -1,16 +1,21 @@
 {
   system ? builtins.currentSystem,
-  baseRev ? "72e7fd797e15eaf3f13d3c1d4e80a1e3dfb0ecdf",
+  baseRev
 }:
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/${baseRev}.tar.gz";
+  baseNixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/${baseRev}.tar.gz";
 
-  pkgs = import nixpkgs {
+  pinnedNixpkgsInfo = builtins.fromJSON (builtins.readFile (baseNixpkgs + "/ci/pinned-nixpkgs.json"));
+  pinnedNixpkgs = fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${pinnedNixpkgsInfo.rev}.tar.gz";
+    inherit (pinnedNixpkgsInfo) sha256;
+  };
+
+  pkgs = import pinnedNixpkgs {
     inherit system;
     config = {};
     overlays = [];
   };
-  prsByFilePath = ./prs-by-file;
 
   nixfmtSrc = fetchTarball {
     url = "https://github.com/NixOS/nixfmt/archive/99829a0b149eab4d82764fb0b5d4b6f3ea9a480d.tar.gz";
@@ -18,13 +23,34 @@ let
   };
   nixfmt = (import nixfmtSrc { inherit system; }).packages.nixfmt;
 
+  nix = pkgs.nixVersions.latest.overrideAttrs (old: {
+    patches = old.patches or [] ++ [
+      # https://github.com/NixOS/nix/pull/11120
+      (pkgs.fetchpatch {
+        url = "https://github.com/NixOS/nix/commit/9fae50ed4be6c7f8bd16ece9626709d78bb4b01c.patch";
+        hash = "sha256-2Z9UxT3m/m3acpRiqjGHd3bKjUz78/6IU8susrk59OU=";
+      })
+      (pkgs.fetchpatch {
+        url = "https://github.com/NixOS/nix/commit/23c44d98664b81f06259fc785a31dfcf6d7c1a9f.patch";
+        hash = "sha256-BMMM0SPzrPmsnmCaezuqjx4Yif5IyV96CPsAzGnBvvY=";
+      })
+      # https://github.com/NixOS/nix/pull/11123
+      (pkgs.fetchpatch {
+        url = "https://github.com/NixOS/nix/commit/94975d4625b464609a2e5cd7e4862375518bf4ec.patch";
+        hash = "sha256-2QE204IqKk0x0htD9nuBOCqxnGVAdeBaTthcV+lR8ok=";
+      })
+    ];
+  });
+
+  prsByFilePath = ./prs-by-file;
+
   files = pkgs.runCommand "files" {
-      nativeBuildInputs = [
-        pkgs.fd
-      ];
+    nativeBuildInputs = [
+      pkgs.fd
+    ];
   } ''
     mkdir $out
-    cd ${nixpkgs}
+    cd ${baseNixpkgs}
 
     # All Nix files
     fd -t f -e nix \
@@ -44,7 +70,6 @@ let
   processFile = pkgs.writeShellScript "process" ''
     set -euo pipefail
 
-    #echo "Formatting file $1" >&2
     before=$(nix-instantiate --parse "$1")
     nixfmt --verify "$1"
     after=$(nix-instantiate --parse "$1")
@@ -58,32 +83,17 @@ let
   formatted = pkgs.runCommand "nixpkgs-formatted"
     {
       nativeBuildInputs = [
-        (pkgs.nixVersions.latest.overrideAttrs (old: {
-          patches = old.patches or [] ++ [
-            # https://github.com/NixOS/nix/pull/11120
-            (pkgs.fetchpatch {
-              url = "https://github.com/NixOS/nix/commit/9fae50ed4be6c7f8bd16ece9626709d78bb4b01c.patch";
-              hash = "sha256-2Z9UxT3m/m3acpRiqjGHd3bKjUz78/6IU8susrk59OU=";
-            })
-            (pkgs.fetchpatch {
-              url = "https://github.com/NixOS/nix/commit/23c44d98664b81f06259fc785a31dfcf6d7c1a9f.patch";
-              hash = "sha256-BMMM0SPzrPmsnmCaezuqjx4Yif5IyV96CPsAzGnBvvY=";
-            })
-            # https://github.com/NixOS/nix/pull/11123
-            (pkgs.fetchpatch {
-              url = "https://github.com/NixOS/nix/commit/94975d4625b464609a2e5cd7e4862375518bf4ec.patch";
-              hash = "sha256-2QE204IqKk0x0htD9nuBOCqxnGVAdeBaTthcV+lR8ok=";
-            })
-          ];
-        }))
+        # TODO: Replace with pkgs.nixfmt-rfc-style once the pinned Nixpkgs has been updated in master:
+        # https://github.com/NixOS/nixpkgs/tree/master/ci#pinned-nixpkgs
         nixfmt
+        nix
         pkgs.gitMinimal
         pkgs.parallel
       ];
     }
     ''
       set -euo pipefail
-      cp -r --no-preserve=mode ${nixpkgs} $out
+      cp -r --no-preserve=mode ${baseNixpkgs} $out
       cd $out
       export NIX_STATE_DIR=$(mktemp -d)
       nix-store --init
@@ -108,76 +118,36 @@ let
 
     This can be verified using
 
-        nix-build https://gist.github.com/infinisil/4b7a1a1e5db681d04446e73e39048aec/archive/$Format:%H$.tar.gz -A applyFormatting
+        nix-build https://gist.github.com/infinisil/4b7a1a1e5db681d04446e73e39048aec/archive/$Format:%H$.tar.gz \
+          --argstr baseRev ${baseRev}
         result/bin/apply-formatting $NIXPKGS_PATH
   '';
 
-  applyFormatting = pkgs.writeShellApplication {
-    name = "apply-formatting";
-    excludeShellChecks = [ "SC2016" ];
-    text = ''
-      nixpkgs=$1
-
-      tmp=$(mktemp -d)
-      cleanup() {
-        # Don't exit early if anything fails to cleanup
-        set +o errexit
-        [[ -e "$tmp/formatted" ]] && git -C "$nixpkgs" worktree remove --force "$tmp/formatted"
-        rm -rf "$tmp"
-      }
-      trap cleanup exit
-
-      git -C "$nixpkgs" worktree add "$tmp/formatted" "${baseRev}"
-
-      rsync -r ${formatted}/ "$tmp/formatted"
-      git -C "$tmp/formatted" commit -a -m ${pkgs.lib.escapeShellArg message}
-      formattedRev=$(git -C "$tmp/formatted" rev-parse HEAD)
-      echo -e "\n# treewide: Nix format pass 1\n$formattedRev" >> "$tmp/formatted/.git-blame-ignore-revs"
-      git -C "$tmp/formatted" commit -a -m ".git-blame-ignore-revs: Add treewide Nix format"
-      finalRev=$(git -C "$tmp/formatted" rev-parse HEAD)
-      git -C "$nixpkgs" diff HEAD.."$finalRev" || true
-      echo "Final revision: $finalRev (you can use this in e.g. \`git reset --hard\`)"
-    '';
-  };
-
-  updateFiles = pkgs.writeShellApplication {
-    name = "update-files-to-format";
-    runtimeInputs = with pkgs; [
-      github-cli
-      jq
-      fd
-    ];
-    text = ''
-      tmp=$(mktemp -d)
-      trap 'rm -rf "$tmp"' exit
-
-      {
-        for day in $(seq 0 60); do
-          date=$(date --date="$day days ago" -I -u)
-          echo "Fetching PRs from $date" >&2
-          result=$(gh pr list -R NixOS/nixpkgs --limit 500 --search "updated:$date -label:\"2.status: merge conflict\"" --json files,number |
-            jq '.[] | select(.number != 322537)' -c)
-          count=$(jq -s 'length' <<< "$result")
-          echo "Got $count PRs" >&2
-          echo -n "$result"
-        done
-      } |
-        jq -s 'map(
-          .number as $number |
-            .files[] |
-            { number: $number, path: .path }
-          ) |
-          reduce .[] as $item ({};
-            . "\($item.path)" += [ $item.number ]
-          ) |
-          to_entries |
-          sort_by(.value | length) |
-          .[] |
-          "\(.key) \(.value | join(" "))"
-        ' -r > "${toString prsByFilePath}"
-    '';
-  };
 in
-{
-  inherit updateFiles applyFormatting;
+pkgs.writeShellApplication {
+  name = "apply-formatting";
+  excludeShellChecks = [ "SC2016" ];
+  text = ''
+    nixpkgs=$1
+
+    tmp=$(mktemp -d)
+    cleanup() {
+      # Don't exit early if anything fails to cleanup
+      set +o errexit
+      [[ -e "$tmp/formatted" ]] && git -C "$nixpkgs" worktree remove --force "$tmp/formatted"
+      rm -rf "$tmp"
+    }
+    trap cleanup exit
+
+    git -C "$nixpkgs" worktree add "$tmp/formatted" "${baseRev}"
+
+    rsync -r ${formatted}/ "$tmp/formatted"
+    git -C "$tmp/formatted" commit -a -m ${pkgs.lib.escapeShellArg message}
+    formattedRev=$(git -C "$tmp/formatted" rev-parse HEAD)
+    echo -e "\n# treewide: Nix format pass 1\n$formattedRev" >> "$tmp/formatted/.git-blame-ignore-revs"
+    git -C "$tmp/formatted" commit -a -m ".git-blame-ignore-revs: Add treewide Nix format"
+    finalRev=$(git -C "$tmp/formatted" rev-parse HEAD)
+    git -C "$nixpkgs" diff HEAD.."$finalRev" || true
+    echo "Final revision: $finalRev (you can use this in e.g. \`git reset --hard\`)"
+  '';
 }
---
+++ update.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash --pure -p jq github-cli
+tmp=$(mktemp -d)
+trap 'rm -rf "$tmp"' exit
+
+{
+  for day in $(seq 0 60); do
+    date=$(date --date="$day days ago" -I -u)
+    echo "Fetching PRs from $date" >&2
+    result=$(gh pr list -R NixOS/nixpkgs --limit 500 --search "updated:$date -label:\"2.status: merge conflict\"" --json files,number |
+      jq '.[] | select(.number != 322537)' -c)
+    count=$(jq -s 'length' <<< "$result")
+    echo "Got $count PRs" >&2
+    echo -n "$result"
+  done
+} |
+  jq -s 'map(
+    .number as $number |
+      .files[] |
+      { number: $number, path: .path }
+    ) |
+    reduce .[] as $item ({};
+      . "\($item.path)" += [ $item.number ]
+    ) |
+    to_entries |
+    sort_by(.value | length) |
+    .[] |
+    "\(.key) \(.value | join(" "))"
+  ' -r > "$(dirname "${BASH_SOURCE[0]}")/prs-by-file"
