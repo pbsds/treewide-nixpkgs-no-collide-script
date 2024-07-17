3efdd7a670f5984d52ff4e1a9883d2c45431f8f6
Silvan Mosberger <silvan.mosberger@tweag.io>
2024-07-17 03:18:27 +0200

    Fully implement nix-instantiate --parse checking

--- default.nix
+++ default.nix
@@ -1,10 +1,12 @@
 {
   system ? builtins.currentSystem,
+  baseRev ? "7ffbecde9aa26364ac79010154bf78bb77529fd2",
 }:
-let
-  baseRev = "7ffbecde9aa26364ac79010154bf78bb77529fd2";
-
-  nixpkgs = fetchTarball "https://github.com/tweag/nixpkgs/archive/${baseRev}.tar.gz";
+rec {
+  nixpkgs = fetchTarball {
+    url = "https://github.com/tweag/nixpkgs/archive/${baseRev}.tar.gz";
+    sha256 = "11b7nvmz4qfh6dk0p6cs11qsinpa0xcj99h8m9xd9rbrghf3pxh9";
+  };
 
   pkgs = import nixpkgs {
     inherit system;
@@ -13,43 +15,89 @@ let
   };
   prsByFilePath = ./prs-by-file;
 
+  nixSrc = fetchTarball {
+    url = "https://github.com/NixOS/nix/archive/8f22245fdfee1516614b0eb09538ea7e4448f5bb.tar.gz";
+    sha256 = "1bb4fpb6kdi7vrhwqghl2880rx3pzzxij42izfn4br93ymbi6vgk";
+  };
+
   nixfmtSrc = fetchTarball {
     url = "https://github.com/NixOS/nixfmt/archive/99829a0b149eab4d82764fb0b5d4b6f3ea9a480d.tar.gz";
     sha256 = "0lnl9vlbyrfplmq3hpmpjlmhjdwwbgk900wgi25ib27v0mlgpnxp";
   };
   nixfmt = (import nixfmtSrc { inherit system; }).packages.nixfmt;
 
+  files = pkgs.runCommand "files" {
+      nativeBuildInputs = [
+        pkgs.fd
+      ];
+  } ''
+    mkdir $out
+    cd ${nixpkgs}
+
+    # All Nix files
+    fd -t f -e nix \
+      | sort > "$out"/all-files
+
+    # All files touched by recent PRs
+    cut -d' ' -f1 "${prsByFilePath}" |
+      sort -u > "$out/pr-files"
+
+    # All Nix files that haven't been touched by recent PRs
+    comm -23 "$out"/all-files "$out"/pr-files > "$out/files-to-format"
+
+    # The number of those
+    wc -l < $out/files-to-format > $out/count
+  '';
+
+  processFile = pkgs.writeShellScript "process" ''
+    set -euo pipefail
+
+    #echo "Formatting file $1" >&2
+    before=$(nix-instantiate --parse "$1")
+    nixfmt --verify "$1"
+    after=$(nix-instantiate --parse "$1")
+    if [[ "$before" != "$after" ]]; then
+      echo "$1 parses differently after formatting:" >&2
+      git -P diff --no-index --word-diff <(cat <<< "$before") <(cat <<< "$after") || true
+      exit 255
+    fi
+  '';
+
   formatted = pkgs.runCommand "nixpkgs-formatted"
     {
       nativeBuildInputs = [
+        (pkgs.nixVersions.latest.overrideAttrs (old: {
+          src = nixSrc;
+          patches = old.patches or [] ++ [
+            # https://github.com/NixOS/nix/pull/11120
+            (pkgs.fetchpatch {
+              url = "https://github.com/NixOS/nix/commit/9fae50ed4be6c7f8bd16ece9626709d78bb4b01c.patch";
+              hash = "sha256-2Z9UxT3m/m3acpRiqjGHd3bKjUz78/6IU8susrk59OU=";
+            })
+            (pkgs.fetchpatch {
+              url = "https://github.com/NixOS/nix/commit/23c44d98664b81f06259fc785a31dfcf6d7c1a9f.patch";
+              hash = "sha256-BMMM0SPzrPmsnmCaezuqjx4Yif5IyV96CPsAzGnBvvY=";
+            })
+            # https://github.com/NixOS/nix/pull/11123
+            (pkgs.fetchpatch {
+              url = "https://github.com/NixOS/nix/commit/94975d4625b464609a2e5cd7e4862375518bf4ec.patch";
+              hash = "sha256-2QE204IqKk0x0htD9nuBOCqxnGVAdeBaTthcV+lR8ok=";
+            })
+          ];
+        }))
         nixfmt
-        pkgs.fd
-        pkgs.nix
+        pkgs.gitMinimal
+        pkgs.parallel
       ];
     }
     ''
-      tmp=$(mktemp -d)
-
+      set -euo pipefail
       cp -r --no-preserve=mode ${nixpkgs} $out
       cd $out
-
-      fd -t f -e nix \
-        | sort > "$tmp"/all-files
-
-      cut -d' ' -f1 "${prsByFilePath}" |
-        sort -u > "$tmp/pr-files"
-
-      comm -23 "$tmp"/all-files "$tmp"/pr-files > "$tmp/files-to-format"
-
-      xargs -P "$NIX_BUILD_CORES" -a "$tmp/files-to-format" nixfmt
-
       export NIX_STATE_DIR=$(mktemp -d)
+      nix-store --init
 
-      while IFS= read -r file; do
-        if [[ "$(nix-instantiate --parse "''${nixpkgs}/$file")" != "$(nix-instantiate --parse ./"$file")" ]]; then
-          echo "$file parses differently after formatting"
-        fi
-      done < "$tmp/files-to-format"
+      parallel --bar -P "$NIX_BUILD_CORES" -a "${files}/files-to-format" ${processFile} {}
     '';
 
   message = ''
@@ -72,34 +120,35 @@ let
         nix-build https://gist.github.com/infinisil/4b7a1a1e5db681d04446e73e39048aec/archive/$Format:%H$.tar.gz
         result/bin/check-formatting $NIXPKGS_PATH
   '';
-in
-pkgs.writeShellApplication {
-  name = "check-formatting";
-  excludeShellChecks = [ "SC2016" ];
-  text = ''
-    nixpkgs=$1
 
-    tmp=$(mktemp -d)
-    cleanup() {
-      # Don't exit early if anything fails to cleanup
-      set +o errexit
-      [[ -e "$tmp/formatted" ]] && git -C "$nixpkgs" worktree remove --force "$tmp/formatted"
-      rm -rf "$tmp"
-    }
-    trap cleanup exit
+  applyFormatting = pkgs.writeShellApplication {
+    name = "check-formatting";
+    excludeShellChecks = [ "SC2016" ];
+    text = ''
+      nixpkgs=$1
 
-    git -C "$nixpkgs" worktree add "$tmp/formatted" "${baseRev}"
+      tmp=$(mktemp -d)
+      cleanup() {
+        # Don't exit early if anything fails to cleanup
+        set +o errexit
+        [[ -e "$tmp/formatted" ]] && git -C "$nixpkgs" worktree remove --force "$tmp/formatted"
+        rm -rf "$tmp"
+      }
+      trap cleanup exit
 
-    rsync -r ${formatted}/ "$tmp/formatted"
-    git -C "$tmp/formatted" commit -a -m ${pkgs.lib.escapeShellArg message}
-    formattedRev=$(git -C "$tmp/formatted" rev-parse HEAD)
-    echo -e "\n# treewide: Nix format pass 1\n$formattedRev" >> "$tmp/formatted/.git-blame-ignore-revs"
-    git -C "$tmp/formatted" commit -a -m ".git-blame-ignore-revs: Add treewide Nix format"
-    finalRev=$(git -C "$tmp/formatted" rev-parse HEAD)
-    git -C "$nixpkgs" diff HEAD.."$finalRev" || true
-    echo "Final revision: $finalRev (you can use this in e.g. \`git reset --hard\`)"
-  '';
-} // {
+      git -C "$nixpkgs" worktree add "$tmp/formatted" "${baseRev}"
+
+      rsync -r ${formatted}/ "$tmp/formatted"
+      git -C "$tmp/formatted" commit -a -m ${pkgs.lib.escapeShellArg message}
+      formattedRev=$(git -C "$tmp/formatted" rev-parse HEAD)
+      echo -e "\n# treewide: Nix format pass 1\n$formattedRev" >> "$tmp/formatted/.git-blame-ignore-revs"
+      git -C "$tmp/formatted" commit -a -m ".git-blame-ignore-revs: Add treewide Nix format"
+      finalRev=$(git -C "$tmp/formatted" rev-parse HEAD)
+      git -C "$nixpkgs" diff HEAD.."$finalRev" || true
+      echo "Final revision: $finalRev (you can use this in e.g. \`git reset --hard\`)"
+    '';
+  };
+
   updateFiles = pkgs.writeShellApplication {
     name = "update-files-to-format";
     runtimeInputs = with pkgs; [
